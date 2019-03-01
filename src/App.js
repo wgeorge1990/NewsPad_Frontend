6101befc02e287307c9dca4b1d59f0b8386b88b7
wgeorge1990 <williameverettgeorge@icloud.com>
2019-03-01 09:53:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Button } from 'semantic-ui-react'
+
+
+const key = "mn3dtRxwdGYGdziMyPqLiOgfsQ08gwAb"	
+const mostViewed = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${key}`
 
 class App extends Component {
+  state = {
+    articles: []
+  }
+  componentDidMount = () => {
+    fetch(mostViewed).then(res => res.json()).then(data => 
+      this.setState({
+        articles: data
+      }))
+  }
+
+  articleCard = () => (
+    <Card>
+      <a className="embedly-card"
+        href="https://www.nytimes.com/interactive/2019/02/27/us/politics/michael-cohen-testimony.html">
+        Michael D. Cohen's Congressional Testimony</a>
+        <Button>
+          Add To Favorites
+        </Button>
+        
+
+    </Card>
+  )
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.articleCard()}
+        {this.articleCard()}
       </div>
     );
   }
